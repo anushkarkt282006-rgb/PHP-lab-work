dbd5c4cfc667455a0c562975f6ee9d6887c7f3e7
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-07-09 08:40:39 +0530

    Add functionality to retrieve and display data

---
+++ get_data.php
@@ -0,0 +1,14 @@
+<?php
+
+//DEbugging Data 
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+// $user_name = $_GET['u_name'];
+//$user_surname=$_GET['u_pass'];
+
+//echo "User Name: ".$user_name."<br>";
+//echo "User Surname: ".$user_surname."<br>";
+
+?>
