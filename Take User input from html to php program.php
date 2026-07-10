a7b839d1b5a27dcd236f1db9e97b28c7b023cfde
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-07-10 22:38:09 +0530

    Add User Input Form

---
+++ Take User input from html to php program.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Input Form</title>
+</head>
+<body>
+
+<h2>Student Information</h2>
+
+<form action="display.php" method="post">
+    Name:
+    <input type="text" name="name"><br><br>
+
+    Age:
+    <input type="number" name="age"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
