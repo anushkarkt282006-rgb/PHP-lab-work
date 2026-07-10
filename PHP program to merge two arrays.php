b8beebff9c384dae1047d73c13f9b510b0207643
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-07-10 23:42:07 +0530

    Add PHP program to merge two arrays

---
+++ PHP program to merge two arrays.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Merge Arrays</title>
+</head>
+<body>
+
+<form action="merge.php" method="post">
+
+First Array:
+<input type="text" name="arr1"><br><br>
+
+Second Array:
+<input type="text" name="arr2"><br><br>
+
+<input type="submit" value="Merge">
+
+</form>
+
+</body>
+</html>
