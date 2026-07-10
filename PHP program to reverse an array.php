c32a40d8ab57a86ad8c5ba6bbf50dd06680b775b
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-07-10 23:40:31 +0530

    Add PHP program to reverse an array

---
+++ PHP program to reverse an array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reverse Array</title>
+</head>
+<body>
+
+<form action="reverse.php" method="post">
+    Enter values (comma separated):
+    <input type="text" name="arr">
+    <input type="submit" value="Reverse">
+</form>
+
+</body>
+</html>
