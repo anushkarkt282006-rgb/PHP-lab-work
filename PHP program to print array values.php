ddd9c076074dbc4664abadd702042ebc9be5c32b
anushkarkt282006 <anushka.rkt282006@gmail.com>
2026-07-10 23:09:48 +0530

    Add PHP program to print array values

---
+++ PHP program to print array values.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Array Input</title>
+</head>
+<body>
+
+<h2>Enter Array Values</h2>
+
+<form action="array_print.php" method="post">
+    Enter values (comma separated):
+    <input type="text" name="arr">
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
